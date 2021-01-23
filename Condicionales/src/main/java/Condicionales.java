a22df368164a6196d779595212cb13f6c9ad5556
Kevin <jose.aco1838@uppuebla.edu.mx>
2021-01-23 10:13:17 -0600

    condicionales

--- Condicionales/src/main/java/Condicionales.java
+++ Condicionales/src/main/java/Condicionales.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author kevin09
- */
 public class Condicionales {
-    
+public static void main(String args[]){
+System.out.println("Hola Mundo");
+
 }
+}
+
