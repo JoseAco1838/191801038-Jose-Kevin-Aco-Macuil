5d9a913266a01dfef35e6ae57ce844d69525bcd7
jose.aco1838@uppuebla.edu.mx <jose.aco1838@uppuebla.edu.mx>
2021-01-25 08:38:45 -0600

    Promedio

---
+++ Ejercicio3/src/main/java/Ejercicio3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ejercicio3 {
+    public static void main(String[] args) {
+ Scanner scanner=new Scanner(System.in) ;
+         System.out.println("Hola humano dame el numero de tu calificacion: ");
+         double numeroUno=scanner.nextDouble();
+         System.out.println("Humano dame el segundo numero de tu calificacion: ");
+         scanner.nextLine();
+         double numeroDos=scanner.nextDouble();
+         System.out.println("Humano dame el tercer numero de tu calificacion: ");
+         scanner.nextLine();
+         double numeroTres=scanner.nextDouble();
+         System.out.println("A ver tu promedio es: "+((numeroUno +numeroDos+numeroTres)/3));
+    }
+    
+}
